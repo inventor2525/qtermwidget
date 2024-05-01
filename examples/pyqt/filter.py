2324ba9b3a7ee1863eeeccd9a3da8946197bf916
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2024-05-01 14:53:29 -0400

    reduced 'filter' example with text input

--- examples/pyqt/filter.py
+++ examples/pyqt/filter.py
@@ -23,23 +23,36 @@ class MainWindow(QtWidgets.QWidget):
 		# Initialize the terminal
 		self.terminal = Terminal("bash", [])
 		self.layout.addWidget(self.terminal)
-
+		
+		
+		self.buttons_layout = QtWidgets.QHBoxLayout()
+		self.layout.addLayout(self.buttons_layout)
+		
 		# Add a button to send 'a' key press to the terminal
-		self.button = QtWidgets.QPushButton("Send 'a'")
-		self.button.clicked.connect(self.send_a_to_terminal)
-		self.layout.addWidget(self.button)
+		self.send_button = QtWidgets.QPushButton("Send 'a'")
+		self.send_button.clicked.connect(self.send_to_terminal)
+		self.buttons_layout.addWidget(self.send_button)
+		
+		# Add a toggle button to block all key presses to the terminal
+		self.block_button = QtWidgets.QToolButton()
+		self.block_button.setText("Block All Key Presses")
+		self.block_button.setCheckable(True)
+		def toggle_block(checked):
+			app.should_block = checked
+		self.block_button.toggled.connect(toggle_block)
+		self.buttons_layout.addWidget(self.block_button)
 		
 		#horizontal layout for input and output textfield debug views:
-		self.h_layout = QtWidgets.QHBoxLayout()
-		self.layout.addLayout(self.h_layout)
+		self.text_edits_layout = QtWidgets.QHBoxLayout()
+		self.layout.addLayout(self.text_edits_layout)
 		
 		#input text edit:
 		self.in_text_edit = QtWidgets.QTextEdit()
-		self.h_layout.addWidget(self.in_text_edit)
+		self.text_edits_layout.addWidget(self.in_text_edit)
 		
 		#output text edit:
 		self.out_text_edit = QtWidgets.QTextEdit()
-		self.h_layout.addWidget(self.out_text_edit)
+		self.text_edits_layout.addWidget(self.out_text_edit)
 		
 		#register received data signal:
 		self.terminal.receivedData.connect(self.on_received_data)
@@ -54,7 +67,7 @@ class MainWindow(QtWidgets.QWidget):
 	def on_send_data(self, data, size):
 		self.in_text_edit.append(data)
 
-	def send_a_to_terminal(self):
+	def send_to_terminal(self):
 		event = QtGui.QKeyEvent(QtCore.QEvent.KeyPress, QtCore.Qt.Key_C, QtCore.Qt.ControlModifier)
 		# self.terminal.sendKeyEvent(event)
 		# def clear():
@@ -68,42 +81,24 @@ class MainWindow(QtWidgets.QWidget):
 class MyApp(QtWidgets.QApplication):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-		# self.installEventFilter(self)
-		self.term_has_focus = False
+		self.installEventFilter(self)
+		self.should_block = False
 	
-	def notify(self, receiver, event):
-		if event.type() == QtCore.QEvent.KeyPress:
-			event_type = {v: k for k, v in vars(QtCore.QEvent).items() if not k.startswith('__')}.get(event.type())
-			if receiver.parent():
-				print(f"Event {event_type} sent to {receiver.objectName()} {receiver.__class__} {receiver.parent()}")
-		return super().notify(receiver, event)
-
-
 	def eventFilter(self, obj, event):
-		if event.type() == QtCore.QEvent.KeyPress:
-			# if self.term_has_focus:
+		if self.should_block:
 			if isinstance(obj.parent(), QTermWidget):
-				print("Term focused: ", type(obj))
+				return True
+		if event.type() == QtCore.QEvent.KeyPress:
 				key = event.key()
 				modifiers = event.modifiers()
 				text = event.text()
 				
-				# if key == QtCore.Qt.Key_A:
-				# 	return True
-			else:
-				print("Term not focused: ", type(obj))
+				if self.should_block:
+					return True
 		return super().eventFilter(obj, event)
 
 if __name__ == "__main__":
 	app = MyApp([])
 	mainWindow = MainWindow()
-	def on_focus():
-		print("Terminal has focus")
-		app.term_has_focus = True
-	def on_lost_focus():
-		print("Terminal lost focus")
-		app.term_has_focus = False
-	mainWindow.terminal.termLostFocus.connect(on_lost_focus)
-	mainWindow.terminal.termGetFocus.connect(on_focus)
 	mainWindow.show()
 	app.exec_()
